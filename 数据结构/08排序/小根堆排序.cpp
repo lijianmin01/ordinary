f71172130c0474ec97470b0e9950913189fd92d3
lijianmin01 <lijianmin01@126.com>
2021-05-17 10:57:52 +0800

    123

--- 数据结构/08排序/小根堆排序.cpp
+++ 数据结构/08排序/小根堆排序.cpp
@@ -6,45 +6,44 @@ void swap(int &a,int &b){
     b = temp;
 }
 
-/*将以k为跟的子树调整为大根堆*/
-void HeadAdjust(int A[],int k,int len){
+/*以k为根的子树，调整为小根堆*/
+void HeadAdjustMin(int A[],int k,int len){
     int i,j;
-    /*A[0] 暂存根节点*/
     A[0]=A[k];
-    for(i=2*k;i<=len;i*=2){
-        if(i<len&&A[i]<A[i+1]){
+    for ( i = 2*k; i <= len; i*=2)
+    {
+        if(i<len&&A[i]>A[i+1]){
             i++;
         }
-        if(A[0]>=A[i]){
+        if(A[0]<=A[i]){
             break;
         }else{
             A[k]=A[i];
-            k=i;
+            k = i;
         }
     }
     A[k]=A[0];
 }
-/*建立大根堆*/
-void BuildMaxHeap(int A[],int len){
+
+// 建立小根堆
+void BuildMinHeap(int A[],int len){
     int i;
-    for ( i = len/2; i >0; i--)
+    for ( i = len/2; i >=1 ; i--)
     {
-        HeadAdjust(A,i,len);
+        HeadAdjustMin(A,i,len);
     }
     
 }
 
+// 利用小根堆进行排序
 void HeapSort(int A[],int len){
-    BuildMaxHeap(A,len);
+    BuildMinHeap(A,len);
     int i;
-    for ( i = len; i > 1; i--)
-    {
-        swap(A[i],A[1]);
-        HeadAdjust(A,1,i-1);
+    for(i=len;i>1;i--){
+        swap(A[1],A[i]);
+        HeadAdjustMin(A,1,i-1);
     }
-    
 }
-
 
 int main(void){
     int n;
@@ -53,7 +52,7 @@ int main(void){
     int i,j;
     for ( i = 0; i < n; i++)
     {
-        scanf("%d",nums[i+1]);
+        scanf("%d",&nums[i+1]);
     }
     HeapSort(nums,n);
     for ( i = 0; i < n; i++)
